0960f2c4ecec9eceaf1a9fc153a56d6a6d97cf6a
throwmeplease <140489796+throwmeplease@users.noreply.github.com>
2023-10-04 12:18:59 +0530

    Update app.py

--- Client_Python_Modules/app.py
+++ Client_Python_Modules/app.py
@@ -4,7 +4,7 @@ from Client_Python_Modules.page import *
 class App(tk.Tk):
     height = 720
     width = 1080
-    iconPath = 'Images/favicon.ico'
+    # iconPath = 'Images/favicon.ico'
     pages = []
 
     def __init__(self):
@@ -12,31 +12,40 @@ class App(tk.Tk):
         self.title('Restaurant management system')
         self.geometry(str(self.width) + 'x' + str(self.height))
         self.resizable(False, False)
-        self.iconbitmap(self.iconPath)
+        # self.iconbitmap(self.iconPath)
         self.createPages()
         self.pages[0].pack()
 
     def createPages(self):
         startPage = Page(self, 'start page')
-        heading = ttk.Label(
+        ttk.Label(
             master=startPage,
             text='Name_Of_Restaurant'
-        )
-        heading.pack()
-        signIn = ttk.Button(
+        ).pack()
+        ttk.Button(
             master=startPage,
             text='Sign in',
             command=lambda: self.switch(1)
-        )
-        signUp = ttk.Button(
+        ).pack()
+        ttk.Button(
             master=startPage,
             text='sign up',
             command=lambda: self.switch(2)
-        )
-        signIn.pack()
-        signUp.pack()
+        ).pack()
+        ttk.Button(
+            master=startPage,
+            text='Staff',
+            command=self.staffLogin
+        ).pack()
+        ttk.Button(
+            master=startPage,
+            text='guest',
+            command=lambda: self.switch(1,True)
+        ).pack()
         self.pages.append(startPage)
 
-    def switch(self, pageNo: int):
+    def switch(self, pageNo: int, popup:bool = False):
         print('switching to', pageNo)
         pass
+    def staffLogin(self):
+        print('switch to staff site')
