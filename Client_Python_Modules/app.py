5c6bb752dcd40f306a6168effc65795c7d2b6ef8
Keshav <keshavdsingh2905@gmail.com>
2023-10-07 22:21:44 +0530

    redid the graphics managing system

--- Client_Python_Modules/app.py
+++ Client_Python_Modules/app.py
@@ -18,34 +18,48 @@ class App(tk.Tk):
 
     def createPages(self):
         startPage = Page(self, 'start page')
-        ttk.Label(
+        ui_obj = ttk.Label(
             master=startPage,
             text='Name_Of_Restaurant'
-        ).pack()
-        ttk.Button(
+        )
+        ui_obj.add = lambda me=ui_obj: me.pack()
+        startPage.addChild(ui_obj)
+        ui_obj = ttk.Button(
             master=startPage,
             text='Sign in',
             command=lambda: self.switch(1)
-        ).pack()
-        ttk.Button(
+        )
+        ui_obj.add = lambda me=ui_obj: me.pack()
+        startPage.addChild(ui_obj)
+        ui_obj = ttk.Button(
             master=startPage,
             text='sign up',
             command=lambda: self.switch(2)
-        ).pack()
-        ttk.Button(
+        )
+        ui_obj.add = lambda me=ui_obj: me.pack()
+        startPage.addChild(ui_obj)
+        ui_obj = ttk.Button(
             master=startPage,
             text='Staff',
             command=self.staffLogin
-        ).pack()
-        ttk.Button(
+        )
+        ui_obj.add = lambda me=ui_obj: me.pack()
+        startPage.addChild(ui_obj)
+        ui_obj = ttk.Button(
             master=startPage,
             text='guest',
-            command=lambda: self.switch(1,True)
-        ).pack()
+            command=lambda: self.switch(1, True)
+        )
+        ui_obj.add = lambda me=ui_obj: me.pack()
+        startPage.addChild(ui_obj)
         self.pages.append(startPage)
 
-    def switch(self, pageNo: int, popup:bool = False):
-        print('switching to', pageNo)
-        pass
+
+    def switch(self, pageNo: int, popup: bool = False):
+        if popup:
+            print('Popup no :', pageNo)
+        else:
+            print('switching to', pageNo)
+
     def staffLogin(self):
         print('switch to staff site')
--- Client_Python_Modules/page.py
+++ Client_Python_Modules/page.py
@@ -2,7 +2,29 @@ import tkinter as tk
 from tkinter import ttk
 
 
-class Page(ttk.Frame):
+class UiBlock(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master=master)
+        self.childs = []
+
+    def addChild(self, obj) -> None:
+        self.childs.append(obj)
+
+    def pack(self) -> None:
+        super().pack()
+        for i in self.childs:
+            i.add()
+
+    def pack_forget(self) -> None:
+        for i in self.childs:
+            i.pack_forget()
+        super().pack_forget()
+
+    def add(self):
+        raise 'Add(Pack) not overwritten error'
+
+
+class Page(UiBlock):
     pageId = ''
 
     def __init__(self, widget, pageId):
