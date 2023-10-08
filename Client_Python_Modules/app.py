c7b4f52042d39092fc17a5278267db9403705814
Keshav <keshavdsingh2905@gmail.com>
2023-10-08 18:14:41 +0530

    redid the page change ... again ... cause I was dumb the first time and added page 2 proper

--- Client_Python_Modules/app.py
+++ Client_Python_Modules/app.py
@@ -1,65 +1,116 @@
+import tkinter
+
 from Client_Python_Modules.page import *
 
 
 class App(tk.Tk):
-    height = 720
-    width = 1080
+    height: int = 720
+    width: int = 1080
+    curPage: int = 0
     # iconPath = 'Images/favicon.ico'
-    pages = []
+    pages: list
+    pageCreatorPrototypes: dict
 
     def __init__(self):
         super().__init__()
+        self.pageCreatorPrototypes = {
+            0: self.makeStartPage,
+            1: self.makeSignIn
+        }
         self.title('Restaurant management system')
         self.geometry(str(self.width) + 'x' + str(self.height))
         self.resizable(False, False)
+        self.pages = [None] * 100
         # self.iconbitmap(self.iconPath)
-        self.createPages()
-        self.pages[0].pack()
+        self.createPage(0)
+        self.pages[self.curPage].pack()
 
-    def createPages(self):
-        startPage = Page(self, 'start page')
-        ui_obj = ttk.Label(
-            master=startPage,
-            text='Name_Of_Restaurant'
-        )
-        ui_obj.add = lambda me=ui_obj: me.pack()
-        startPage.addChild(ui_obj)
-        ui_obj = ttk.Button(
-            master=startPage,
-            text='Sign in',
-            command=lambda: self.switch(1)
-        )
-        ui_obj.add = lambda me=ui_obj: me.pack()
-        startPage.addChild(ui_obj)
-        ui_obj = ttk.Button(
-            master=startPage,
-            text='sign up',
-            command=lambda: self.switch(2)
-        )
-        ui_obj.add = lambda me=ui_obj: me.pack()
-        startPage.addChild(ui_obj)
-        ui_obj = ttk.Button(
-            master=startPage,
-            text='Staff',
-            command=self.staffLogin
-        )
-        ui_obj.add = lambda me=ui_obj: me.pack()
-        startPage.addChild(ui_obj)
-        ui_obj = ttk.Button(
-            master=startPage,
-            text='guest',
-            command=lambda: self.switch(1, True)
-        )
-        ui_obj.add = lambda me=ui_obj: me.pack()
-        startPage.addChild(ui_obj)
-        self.pages.append(startPage)
-
-
-    def switch(self, pageNo: int, popup: bool = False):
+    def switch(self, pageNo: int, popup: bool = False) -> None:
         if popup:
             print('Popup no :', pageNo)
+            self.openPopup(pageNo)
+            return
         else:
             print('switching to', pageNo)
+        self.pages[self.curPage].pack_forget()
+        self.curPage = pageNo
+        try:
+            self.pages[self.curPage].pack()
+        except AttributeError:
+            self.createPage(pageNo)
+            self.pages[self.curPage].pack()
+
+    def openPopup(self, popupno: int) -> None:
+        pass
 
     def staffLogin(self):
         print('switch to staff site')
+
+    def validateSignIn(self, number, passwd):
+        print('checking credentials ....')
+        print(number.get(), '\t:\t', passwd.get())
+        print('if fine: switch, else : popup(message)')
+
+    def createPage(self, pageNo: int = 0):
+        self.pages[pageNo] = self.pageCreatorPrototypes[pageNo]()
+
+    def makeStartPage(self) -> Page:
+        startPage = Page(self, 'start page')
+        ttk.Label(
+            master=startPage,
+            text='Name_Of_Restaurant'
+        ).pack()
+        ttk.Button(
+            master=startPage,
+            text='Sign in',
+            command=lambda: self.switch(1)
+        ).pack()
+        ttk.Button(
+            master=startPage,
+            text='sign up',
+            command=lambda: self.switch(2)
+        ).pack()
+        ttk.Button(
+            master=startPage,
+            text='Staff',
+            command=self.staffLogin
+        ).pack()
+        ttk.Button(
+            master=startPage,
+            text='guest',
+            command=lambda: self.switch(1, True)
+        ).pack()
+        return startPage
+
+    def makeSignIn(self) -> Page:
+        signIn = Page(self, 'sign in')
+        ttk.Button(
+            master=signIn,
+            text='back',
+            command=lambda: self.switch(0)
+        ).pack()
+        ttk.Label(
+            master=signIn,
+            text='Number'
+        ).pack()
+        phoneNumber = tkinter.StringVar()
+        ttk.Entry(
+            master=signIn,
+            textvariable=phoneNumber
+        ).pack()
+        ttk.Label(
+            master=signIn,
+            text='Password'
+        ).pack()
+        password = tkinter.StringVar()
+        ttk.Entry(
+            master=signIn,
+            textvariable=password,
+            show='*'
+        ).pack()
+        ttk.Button(
+            master=signIn,
+            text='sign in',
+            command=lambda self=self, num=phoneNumber, pwd=password: self.validateSignIn(num, pwd)
+        ).pack()
+        return signIn
--- Client_Python_Modules/page.py
+++ Client_Python_Modules/page.py
@@ -2,29 +2,35 @@ import tkinter as tk
 from tkinter import ttk
 
 
-class UiBlock(ttk.Frame):
-    def __init__(self, master):
-        super().__init__(master=master)
-        self.childs = []
+# class UiBlock(ttk.Frame):
+#     def __init__(self, master):
+#         super().__init__(master=master)
+#         self.childs = []
+#
+#     def addChild(self, obj) -> None:
+#         self.childs.append(obj)
+#
+#     def pack(self) -> None:
+#         super().pack()
+#         for i in self.childs:
+#             i.add()
+#         self.onEntry()
+#
+#
+#     def pack_forget(self) -> None:
+#         # for i in self.childs:
+#         #     i.pack_forget()
+#         super().pack_forget()
+#
+#     def add(self) -> None:
+#         raise 'Add(Pack) not overwritten error'
+#
+#     def onEntry(self) -> None:
+#         # for those widgets which need to call some function when done setting up
+#         pass
 
-    def addChild(self, obj) -> None:
-        self.childs.append(obj)
 
-    def pack(self) -> None:
-        super().pack()
-        for i in self.childs:
-            i.add()
-
-    def pack_forget(self) -> None:
-        for i in self.childs:
-            i.pack_forget()
-        super().pack_forget()
-
-    def add(self):
-        raise 'Add(Pack) not overwritten error'
-
-
-class Page(UiBlock):
+class Page(ttk.Frame):
     pageId = ''
 
     def __init__(self, widget, pageId):
