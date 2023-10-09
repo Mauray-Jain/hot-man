2eacdbf348921e69a4f72872e7ca6d8c2e1399e5
Mauray-Jain <maurayjain@gmail.com>
2023-10-09 22:45:44 +0530

    Revert "Minor changes"
    This reverts commit b4cce305f21aef828b347c7526acc46a982555b6.

--- test.py
+++
@@ -1,5 +0,0 @@
-from sqlite3 import DatabaseError
-import mysql.connector
-
-cnx = mysql.connector.connect(user = "root", password = "MJ#LovesMysql", host = "127.0.0.1", database = "hotman")
-cnx.close()
