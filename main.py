277549aa465f33cb659ff664673744399d2d39b4
Mauray-Jain <maurayjain@gmail.com>
2023-11-01 22:44:45 +0530

    DB waaleya

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import socket
 from Middle.api import *
 
 if __name__ == '__main__':
-	HOST: str = "::1" # Loopback for now but will be changed later
-	PORT: int = 5000
-	s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    HOST: str = "::1" # Loopback for now but will be changed later
+    PORT: int = 5000
+    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-	# Connect
-	try:
-		s.connect((HOST, PORT))
-	except ConnectionRefusedError:
-		print("Server Not working")
-		exit(1)
+    # Connect
+    try:
+        s.connect((HOST, PORT))
+    except ConnectionRefusedError:
+        print("Server Not working")
+        exit(1)
 
-	app = App(s)
-	app.mainloop()
+    app = App(s)
+    app.mainloop()
 
-	send(s, {"type": "Close"})
-	s.close()
+    send(s, {"type": "Otp", "number": "98971"})
+    print(recv(s))
+    send(s, {"type": "Close"})
+    s.close()
